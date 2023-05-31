0f251c6cb9a022c46f3ff818d088ac5d7b191c7b
chain <klaus.mika1052@gmail.com>
2023-05-31 18:56:40 -0300

    Componentes do Design System - Componente: TextInput

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Text, TetxProps } from '@ignite-ui/react'
+import { Text, TextProps } from '@ignite-ui/react'
 
 export default {
   title: 'Typography/Text',
@@ -9,11 +9,11 @@ export default {
     children:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nulla ratione dolorem porro at! Placeat facere cum commodi praesentium perferendis vero ea ipsa, quod ex aliquid nobis dicta fuga unde?',
   },
-} as Meta<TetxProps>
+} as Meta<TextProps>
 
-export const Primary: StoryObj<TetxProps> = {}
+export const Primary: StoryObj<TextProps> = {}
 
-export const CustomTag: StoryObj<TetxProps> = {
+export const CustomTag: StoryObj<TextProps> = {
   args: {
     children: 'Strong text',
     as: 'strong',
