73dce5f0cafa5d4dce1f514557524be7e94d9f0b
chain <klaus.mika1052@gmail.com>
2023-06-02 15:44:09 -0300

    fix: rename  subpath to production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ const config = {
   },
   viteFinal: (config, {configType}) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-sytem/'
+      config.base = '/05-design-system/'
     }
     return config
   }
